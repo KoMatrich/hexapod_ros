c248f13b0d2d9936ccc0d9fe182edd6dd215a01f
Martin Kocich <kocichmartin@gmail.com>
2024-03-26 19:10:12 +0100

    FEAT: Source bash scripts

---
+++ bash_scipts/common_setup.sh
@@ -0,0 +1,28 @@
+echo "Running common part"
+echo "=============================="
+
+echo "Using ccache for fast compiling"
+export PATH=/usr/lib/ccache:$PATH
+
+echo "soucing ros noetic std libs"
+source /opt/ros/noetic/setup.bash
+
+if [ -f ./devel/setup.bash ]; then
+    echo "Source dir is build"
+else
+    echo "Souce dir is not build"
+    catkin build
+fi
+
+echo "sourcing catkin_ws"
+source ./devel/setup.bash
+
+echo "setting up MDNS"
+export CONTROL=$(avahi-resolve -n ros-vm.local | awk '{print $2}')
+export ROBOT=$(avahi-resolve -n ros.local | awk '{print $2}')
+
+echo "=============================="
+echo "Robot   IP:$ROBOT"
+echo "Control IP:$CONTROL"
+echo "Common part done"
+echo "=============================="
---
+++ bash_scipts/source_robot.sh
@@ -0,0 +1,20 @@
+source ./bash_scipts/common_setup.sh
+
+#=========================
+# PC SPECIFIC !!!
+#=========================
+# static
+export ROS_IP=$ROBOT
+# dynamic but very slow
+#export ROS_HOSTNAE=ros-vm.local
+#=========================
+# https://github.com/ms-iot/ros_comm/pull/56
+#=========================
+
+export MAIN_NODE=$ROBOT
+export ROS_MASTER_URI="http://$MAIN_NODE:11311/"
+
+echo "=============================="
+echo "Roscore IP:$MAIN_NODE"
+echo "=============================="
+
